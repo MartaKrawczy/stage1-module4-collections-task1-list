bd1585d6ac685c1f067d4a248cbc6f1ff7e12417
MartaKrawczy <martaakrawczykowska@gmail.com>
2023-01-18 17:43:33 +0100

    first commit

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
+
     public ArrayList<String> createArrayList(List<String> sourceList) {
+
+        ArrayList<String> duplicate = new ArrayList<>();
+
+        for(int i = 1; i<=sourceList.size();i++){
+            if ((i % 3)== 0){
+                duplicate.add(sourceList.get(i-1));
+                duplicate.add(sourceList.get(i-1));
+            }
+        }
+        return duplicate;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,48 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-    }
+        int A, B;
+        if (Integer.valueOf(a) < 0) {
+            A = Integer.valueOf(a) * -1;
+        }
+        else {
+            A = Integer.valueOf(a);
+        }
+
+        if (Integer.valueOf(b) < 0) {
+            B = Integer.valueOf(b) * -1;
+        }
+        else {
+            B = Integer.valueOf(b);
+        }
+
+
+        if (A == B) {
+            if (Integer.valueOf(a) < Integer.valueOf(b)) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else if (A > B) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+}
 }
